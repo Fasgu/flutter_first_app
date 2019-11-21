e6b08ada19b3957f86f420f1eed406ddbac0ac8d
aguerra95ugas <aguerra95ugas@gmail.com>
2019-11-21 00:28:33 -0500

    Agregar un stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPaid = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -14,9 +13,23 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPaid.asPascalCase),
+          child: RamdomWords(),
         ),
       ),
     );
   }
+}
+
+class RamdomWordsState extends State<RamdomWords>{
+  @override
+  Widget build(BuildContext context){
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+
+}
+
+class RamdomWords extends StatefulWidget{
+  @override
+  RamdomWordsState createState() => new RamdomWordsState();
 }
